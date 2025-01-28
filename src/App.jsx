73e0ab5909fc99228f20f99b2e52773ef1ef84ac
Hafiz-Omer-code <Omer6688660@gmail.com>
2025-01-28 15:35:43 +0500

    Backup project state

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react' // eslint-disable-line no-unused-vars
+import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Main/Main'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar />
+    <Main />
+    </>
+  )
+}
+
+export default App
